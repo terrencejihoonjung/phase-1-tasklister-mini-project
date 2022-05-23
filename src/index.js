0b0dc9bec17047dc7dbb6bae49f2ecec8bd3eac5
--replace-all <terrencejung928@gmail.com>
2022-05-23 03:42:56 -0700

    finished deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("#create-task-form");
+  const newTask = document.querySelector("#new-task-description")
+  const taskList = document.querySelector("#tasks");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const newListItem = document.createElement("li");
+    const newButton = document.createElement("button");
+
+    newListItem.textContent = newTask.value + " ";
+    newButton.textContent = " REMOVE ";
+
+    newButton.addEventListener("click", function(e) {
+      console.log(e);
+      e.target.parentNode.remove();
+    });
+
+    newListItem.appendChild(newButton);
+    taskList.append(newListItem)
+
+
+
+    e.target.reset();
+  })
+
+
+
 });
